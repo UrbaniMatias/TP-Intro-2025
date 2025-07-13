0ccee397dd4522968d66c42d9a2c683233bf0204
Franco Cristaldo <francocristaldo2004@gmail.com>
2025-07-13 14:10:39 -0300

    se agregaron guard clauses en el constructor de 'Usuario'

--- backend/src/models/usuario.js
+++ backend/src/models/usuario.js
@@ -1,5 +1,14 @@
 class Usuario {
   constructor(id, nombre, constrasenia) {
+    if (typeof id !== "number" || !Number.isInteger(id) || id <= 0)
+      throw new Error("id invalido: debe ser un entero positivo");
+
+    if (typeof nombre !== "string" || nombre.length == 0)
+      throw new Error("nombre invalido: debe ser un string no vacio");
+
+    if (typeof constrasenia !== "string" || constrasenia.length == 0)
+      throw new Error("constrasenia invalida: debe ser un string no vacio");
+
     this.id = id;
     this.nombre = nombre;
     this.constrasenia = constrasenia;
