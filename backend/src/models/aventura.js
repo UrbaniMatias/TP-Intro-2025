e3058ae6f22bb7a8c11f0283f0e54fc835a6f50a
Franco Cristaldo <francocristaldo2004@gmail.com>
2025-07-13 14:11:27 -0300

    --no-message

--- backend/src/models/aventura.js
+++ backend/src/models/aventura.js
@@ -1,13 +1,13 @@
 class Aventura {
   constructor(id, titulo, descripcion) {
     if (typeof id !== "number" || !Number.isInteger(id) || id <= 0)
-      throw new Error("ID invalido: debe ser un entero positivo");
+      throw new Error("id invalido: debe ser un entero positivo");
 
     if (typeof titulo !== "string" || titulo.length == 0)
-      throw new Error("Titulo invalido: debe ser un string no vacio");
+      throw new Error("titulo invalido: debe ser un string no vacio");
 
     if (typeof descripcion !== "string")
-      throw new Error("Descripcion invalida: debe ser un string");
+      throw new Error("descripcion invalida: debe ser un string");
 
     this.id = id;
     this.titulo = titulo;
