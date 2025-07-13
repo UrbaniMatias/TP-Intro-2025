1311492cb231a785ad4833ee3813c4a661057b31
Franco Cristaldo <francocristaldo2004@gmail.com>
2025-07-13 13:52:26 -0300

    se agregaron las clases 'Aventura', 'Usuario' y 'Pagina'

---
+++ backend/src/models/pagina.js
@@ -0,0 +1,11 @@
+class Pagina {
+  constructor(id, id_aventura, titulo, contenido, imagen) {
+    this.id = id;
+    this.id_aventura = id_aventura;
+    this.titulo = titulo;
+    this.contenido = contenido;
+    this.imagen = imagen;
+  }
+}
+
+module.exports = Pagina;
---
+++ backend/src/models/usuario.js
@@ -0,0 +1,9 @@
+class Usuario {
+  constructor(id, nombre, constrasenia) {
+    this.id = id;
+    this.nombre = nombre;
+    this.constrasenia = constrasenia;
+  }
+}
+
+module.exports = Usuario;
