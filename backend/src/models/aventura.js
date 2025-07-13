365e95e54b931136b52f6c12120fc5da5c0ce2eb
Franco Cristaldo <francocristaldo2004@gmail.com>
2025-07-13 14:04:50 -0300

    se agregaron guard clauses en el constructor de 'Aventura'

--- backend/src/models/aventura.js
+++ backend/src/models/aventura.js
@@ -1,5 +1,14 @@
 class Aventura {
   constructor(id, titulo, descripcion) {
+    if (typeof id !== "number" || !Number.isInteger(id) || id <= 0)
+      throw new Error("ID invalido: debe ser un entero positivo");
+
+    if (typeof titulo !== "string" || titulo.length == 0)
+      throw new Error("Titulo invalido: debe ser un string no vacio");
+
+    if (typeof descripcion !== "string")
+      throw new Error("Descripcion invalida: debe ser un string");
+
     this.id = id;
     this.titulo = titulo;
     this.descripcion = descripcion;
