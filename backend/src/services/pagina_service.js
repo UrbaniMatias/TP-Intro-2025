37efd9fb53167a44e0c15a210cdf4d24e30b4560
Franco Cristaldo <francocristaldo2004@gmail.com>
2025-07-13 22:31:03 -0300

    se renombro la carpeta database por services

---
+++ backend/src/services/pagina_service.js
@@ -0,0 +1,16 @@
+const conn = require("./db_connection");
+const Pagina = require("../models/pagina");
+
+// devuelve todas las paginas
+// (la verdad no se para que nos vaya a servir)
+async function getPaginas() {
+    
+}
+
+async function getPaginasByIdAventura(id_aventura) {
+    
+}
+
+async function getPaginaById(id) {
+    
+}
