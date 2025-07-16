38b8589fc5e5c7713307d244fbb0eb88753ae219
Franco Cristaldo <francocristaldo2004@gmail.com>
2025-07-16 11:22:37 -0300

    se implemento usuario_service basico

--- backend/src/services/usuario_service.js
+++ backend/src/services/usuario_service.js
@@ -1,12 +1,43 @@
 const express = require("express");
-const router = express.Router();
+const Usuario = require("../models/usuario");
 
-async function getUsuarios() {
-  
+async function getAllUsuarios() {
+  const res = await conn.query("SELECT * FROM usuarios");
+
+  return res.rows.map((row) => new Usuario(row.id, row.nombre));
 }
 
 async function getUsuarioById(id) {
-  
+  try {
+    const res = await conn.query("SELECT * FROM usuarios WHERE id = $1", [id]);
+
+    if (res.rowCount === 0) throw new Error("Usuario no encontrada");
+
+    return new Usuario(res.rows[0].id, res.rows[0].nombre);
+  } catch (error) {
+    console.error("Error en getUsuarioById:", error);
+    throw error;
+  }
 }
 
-module.exports = router;
+async function createUsuario(nombre, contrasenia) {
+  try {
+    if (!nombre || nombre == "")
+      throw new Error("El nombre debe ser un string no vacio");
+
+    if (!contrasenia || contrasenia == "")
+      throw new Error("La contrasenia debe ser un string no vacio");
+
+    const res = await conn.query(
+      "INSERT INTO usuarios (nombre, contrasenia) VALUES ($1, $2)",
+      [nombre, contrasenia]
+    );
+  } catch (error) {
+    console.error("Error en createUsuario:", err);
+    throw err;
+  }
+}
+
+module.exports = { getAllUsuarios, getUsuarioById, getUsuariosByTitle };
+
+module.exports = { get };
