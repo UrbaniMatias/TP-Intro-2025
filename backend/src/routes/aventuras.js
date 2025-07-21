bb2fc08efcabc71aca8f85874195ebddb6fef240
Franco Cristaldo <francocristaldo2004@gmail.com>
2025-07-20 21:07:09 -0300

    se implemento GET /v1/aventuras

--- backend/src/routes/aventuras.js
+++ backend/src/routes/aventuras.js
@@ -1,9 +1,20 @@
-const express = require("express");
 const router = express.Router();
+const aventura_service = require("../services/aventura_service");
 
 // GET /v1/aventuras
-router.get("/", (req, res) => {
+router.get("/", async (req, res) => {
   res.send("Listar todas las aventuras");
+  try {
+    console.log("Method: GET\nURI: /v1/aventuras");
+
+    const aventuras = await aventura_service.getAllAventuras();
+    const res_body = JSON.stringify(aventuras);
+    console.log(`Response: ${res_body}`);
+    res.send(res_body);
+  } catch (error) {
+    console.error(error);
+    res.status(500);
+  }
 });
 
 module.exports = router;
