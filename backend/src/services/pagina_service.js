a55ab87e5d972578a040ec32a00033dc57355db3
matias urbani <murbani@fi.uba.ar>
2025-07-21 20:55:05 -0300

    se agergan funcioes a paginaservices '

--- backend/src/services/pagina_service.js
+++ backend/src/services/pagina_service.js
@@ -20,20 +20,26 @@ async function getPaginaById(id) {
   }
 }
 
-async function createPagina(title, id_aventura, title, contenido, imagen) {
+async function createPagina(titulo, id_aventura, contenido, imagen, es_inicio) {
   try {
-    if (!id_aventura)
-      throw new Error("El id de la aventura es invalido");
-
-    if (title === "")
+    if (titulo === "")
       throw new Error("El titulo debe ser un string no vacio");
 
+    if (!id_aventura)
+      throw new Error("El id de la aventura es invalido");
+    
     if (contenido === "")
       throw new Error("El contenido debe ser un string no vacio");
+    
+    if (imagen === "")
+      throw new Error("Imagen inválida: debe ser string o null");
+
+    if (typeof es_inicio !== "boolean")
+      throw new Error("es_inicio inválido: debe ser boolean");
 
     const res = await conn.query(
-      "INSERT INTO paginas (id_aventura, title, contenido, imagen) VALUES ($1, $2, $3, $4)",
-      [id_aventura, title, contenido, imagen]
+      "INSERT INTO paginas (id_aventura, title, contenido, imagen, es_inicio) VALUES ($1, $2, $3, $4, $5)",
+      [id_aventura, title, contenido, imagen, es_inicio]
     );
   } catch (error) {
     console.error("Error en createPagina:", error);
@@ -41,8 +47,45 @@ async function createPagina(title, id_aventura, title, contenido, imagen) {
   }
 }
 
-async function updatePaginaById(id, titulo = null, contenido = null, imagen = null) {}
+async function validateIdPagina(id) {
+  return (await conn.query("SELECT 1 FROM pagina WHERE id = $1 LIMIT 1", [id])).rowCount !== 0;
+}
 
-async function deletePaginaById(id) {}
+async function updatePaginaById(id, titulo = null, contenido = null, imagen = null) {
+    try {
+    if (!id)
+      throw new Error("ID de pagina requerido");
+    
+    if (validateIdPagina(id) == false)
+      throw new Error("Pagina no encontrado");
+
+    if (titulo)
+      conn.query("UPDATE pagina SET titulo = $2 WHERE id = $1", [id, titulo]);
+   
+    if (contenido)
+      conn.query("UPDATE pagina SET contenido = $2 WHERE id = $1", [id, contenido]);
+ 
+    if (imagen)
+      conn.query("UPDATE pagina SET imagen = $2 WHERE id = $1", [id, imagen]);
+
+  } catch (error) {
+    console.error("Error en updatePaginaById:", error);
+    throw error;
+  }
+}
+
+
+async function deletePaginaById(id) {  
+  try {
+    const res = await conn.query("DELETE FROM pagina WHERE id = $1", [id]);
+
+    if (res.rowCount === 0)
+      throw new Error("Página no encontrada");
+
+  } catch (error) {
+    console.error("Error en deletePaginaById:", error);
+    throw error;
+  }
+}
 
 module.exports = { getPaginaById, createPagina, updatePaginaById, deletePaginaById };
