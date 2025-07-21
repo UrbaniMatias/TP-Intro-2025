b023a5da38840ff3b741a4d6cad649e9e1f96d53
Franco Cristaldo <francocristaldo2004@gmail.com>
2025-07-21 20:25:14 -0300

    se agregaron logs a modo de debug

--- backend/src/api.js
+++ backend/src/api.js
@@ -1,23 +1,32 @@
+console.log("Cargando variables de entorno");
 require("dotenv").config(); // carga las variables de entorno
 
 const express = require("express");
 
+console.log("Creando la app de express");
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
 // Routers
+console.log("Cargando el router usuario");
 const usuarioRouter = require("./routes/usuario");
-const usuariosRouter = require("./routes/usuarios");
-const aventuraRouter = require("./routes/aventura");
-const aventurasRouter = require("./routes/aventuras");
-
 app.use("/v1/usuario", usuarioRouter);
+
+console.log("Cargando el router usuarios");
+const usuariosRouter = require("./routes/usuarios");
 app.use("/v1/usuarios", usuariosRouter);
+
+console.log("Cargando el router aventura");
+const aventuraRouter = require("./routes/aventura");
 app.use("/v1/aventura", aventuraRouter);
+
+console.log("Cargando el router aventuras");
+const aventurasRouter = require("./routes/aventuras");
 app.use("/v1/aventuras", aventurasRouter);
 
+console.log("Iniciando el puerto");
 app.listen(port, () => {
   console.log(`API escuchando en el puerto ${port}`);
 });
