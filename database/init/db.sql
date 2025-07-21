43dfb8d97c64bc19e97d31d70b58f66c4cd5c6b8
matias urbani <murbani@fi.uba.ar>
2025-07-20 21:19:05 -0300

    cambio de un campo en db

--- database/init/db.sql
+++ database/init/db.sql
@@ -6,7 +6,7 @@ CREATE TABLE usuario (
     contrasenia VARCHAR(100) NOT NULL,
     email VARCHAR(100) UNIQUE NOT NULL,
     fecha_registro TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    rol VARCHAR(20) NOT NULL
+    fecha_de_nacimiento DATE NOT NULL
 );
 
 -- entidad 2
