0b57f53982ed3c6cea765973a69bafe05e29ca09
Franco Cristaldo <francocristaldo2004@gmail.com>
2025-07-21 19:37:01 -0300

    se implemento GET /v1/usuarios

--- backend/src/routes/usuarios.js
+++ backend/src/routes/usuarios.js
@@ -1,9 +1,19 @@
-const express = require("express");
 const router = express.Router();
+const usuario_service = require("../services/usuario_service");
 
 // GET /v1/usuarios
-router.get("/", (req, res) => {
-  res.send("Listar todos los usuarios");
+router.get("/", async (req, res) => {
+  try {
+    console.log("Method: GET\nURI: /v1/usuarios");
+
+    const usuarios = await aventura_service.getAllUsuarios();
+    const res_body = JSON.stringify(usuarios);
+    console.log(`Response: ${res_body}`);
+    res.send(res_body);
+  } catch (error) {
+    console.error(error);
+    res.status(500);
+  }
 });
 
 module.exports = router;
