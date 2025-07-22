733771b519f1f214b4ef13e67351eb8b007b641f
Franco Cristaldo <francocristaldo2004@gmail.com>
2025-07-22 20:51:29 -0300

    --no-message

--- backend/src/models/usuario.js
+++ backend/src/models/usuario.js
@@ -5,20 +5,20 @@ export default class Usuario {
 
     if (typeof nombre !== "string" || nombre.length == 0)
       throw new Error("nombre invalido: debe ser un string no vacio");
-     
+
     if (typeof email !== "string" || email.length == 0)
       throw new Error("email inválido: debe ser un string no nulo");
 
-    if (typeof fecha_registro !== "string" || isNaN(new Date(fecha_registro)))
+    if (typeof fecha_registro !== "string")
       throw new Error("fecha_registro inválida");
 
-    if (typeof fecha_de_nacimiento !== "string" || isNaN(new Date(fecha_de_nacimiento)))
+    if (typeof fecha_de_nacimiento !== "string")
       throw new Error("fecha_de_nacimiento inválida");
 
     this.id = id;
     this.nombre = nombre;
     this.email = email;
-    this.fecha_registro = fecha_registro;
-    this.fecha_de_nacimiento = fecha_de_nacimiento;
+    this.fecha_registro = (new Date(fecha_registro)).toLocaleLowerCase();
+    this.fecha_de_nacimiento = (new Date(fecha_de_nacimiento)).toLocaleLowerCase();
   }
 }
