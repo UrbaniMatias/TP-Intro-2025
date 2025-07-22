e2ab99c28ed93265a3699ed4dd7a5351c87e6311
Franco Cristaldo <francocristaldo2004@gmail.com>
2025-07-22 20:16:23 -0300

    se hicieron async todas las arrow functions de los metodos

--- backend/src/routes/usuario.js
+++ backend/src/routes/usuario.js
@@ -4,11 +4,11 @@ import usuario_service from "../services/usuario_service.js";
 const router = Router();
 
 // POST /v1/usuario
-router.post("/", (req, res) => {
+router.post("/", async (req, res) => {
   try {
     res.send("Crear usuario");
 
-    const nuevo_usuario = usuario_service.createUsuario(
+    const nuevo_usuario = await usuario_service.createUsuario(
       req.body.nombre,
       req.body.contrasenia,
       req.body.email,
@@ -23,27 +23,27 @@ router.post("/", (req, res) => {
 });
 
 // GET /v1/usuario/:id_usuario
-router.get("/:id_usuario", (req, res) => {
+router.get("/:id_usuario", async (req, res) => {
   res.send(`Obtener usuario ${req.params.id_usuario}`);
 });
 
 // GET /v1/usuario/:id_usuario/finales
-router.get("/:id_usuario/finales", (req, res) => {
+router.get("/:id_usuario/finales", async (req, res) => {
   res.send(`Finales del usuario ${req.params.id_usuario}`);
 });
 
 // PUT /v1/usuario/:id_usuario
-router.put("/:id_usuario", (req, res) => {
+router.put("/:id_usuario", async (req, res) => {
   res.send(`Actualizar usuario ${req.params.id_usuario}`);
 });
 
 // PUT /v1/usuario/:id_usuario/finales
-router.put("/:id_usuario/finales", (req, res) => {
+router.put("/:id_usuario/finales", async (req, res) => {
   res.send(`Actualizar finales del usuario ${req.params.id_usuario}`);
 });
 
 // DELETE /v1/usuario/:id_usuario
-router.delete("/:id_usuario", (req, res) => {
+router.delete("/:id_usuario", async (req, res) => {
   res.send(`Eliminar usuario ${req.params.id_usuario}`);
 });
 
