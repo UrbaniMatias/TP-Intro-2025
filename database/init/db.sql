ca75972187a8b55840e45abe6bba1e6bfb926230
matias urbani <murbani@fi.uba.ar>
2025-07-23 18:35:41 -0300

    se agrandan los varchar de las tablas

--- database/init/db.sql
+++ database/init/db.sql
@@ -4,7 +4,7 @@ CREATE TABLE usuario (
     id SERIAL PRIMARY KEY,
     nombre VARCHAR(50) NOT NULL,
     contrasenia VARCHAR(100) NOT NULL,
-    email VARCHAR(100) UNIQUE NOT NULL,
+    email VARCHAR(250) UNIQUE NOT NULL,
     fecha_registro TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     fecha_de_nacimiento DATE NOT NULL
 );
@@ -13,11 +13,11 @@ CREATE TABLE usuario (
 CREATE TABLE aventura (
     id SERIAL PRIMARY KEY,
     titulo VARCHAR(50) NOT NULL,
-    descripcion VARCHAR(200) NULL,
+    descripcion VARCHAR(500) NULL,
     autor_id INT NOT NULL REFERENCES usuario(id),
-    genero VARCHAR(30) NOT NULL,
+    genero VARCHAR(50) NOT NULL,
     fecha_creacion TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    portada VARCHAR(255) NULL,
+    portada VARCHAR(1024) NULL,
     id_pagina_inicial INT NOT NULL REFERENCES pagina(id)
 );
 
@@ -26,15 +26,15 @@ CREATE TABLE pagina (
     id SERIAL PRIMARY KEY,
     id_aventura INT NOT NULL REFERENCES aventura(id),
     titulo VARCHAR(100) NOT NULL,
-    contenido VARCHAR(255) NOT NULL,
-    imagen VARCHAR(255) NULL,
-    imagen_de_fondo VARCHAR(255) NULL
+    contenido VARCHAR(500) NOT NULL,
+    imagen VARCHAR(1024) NULL,
+    imagen_de_fondo VARCHAR(1024) NULL
 );
 
 -- relacion entre paginas
 CREATE TABLE opcion (
     id SERIAL PRIMARY KEY,
-    descripcion VARCHAR(200) NOT NULL,
+    descripcion VARCHAR(500) NOT NULL,
     id_pagina_origen INT NOT NULL REFERENCES pagina(id),
     id_pagina_destino INT REFERENCES pagina(id)
 );
