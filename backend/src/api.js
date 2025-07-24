d9eadc2553a694bdb7ce76367f112ceb2353b447
Franco Cristaldo <francocristaldo2004@gmail.com>
2025-07-24 16:13:06 -0300

    se reagrego api.js

---
+++ backend/src/api.js
@@ -0,0 +1,23 @@
+import express, { json } from "express";
+import dotenv from "dotenv";
+import usuarioRouter from "./routes/usuario.js";
+import usuariosRouter from "./routes/usuarios.js";
+import aventuraRouter from "./routes/aventura.js";
+import aventurasRouter from "./routes/aventuras.js";
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(json());
+
+// Routers
+app.use("/v1/usuario", usuarioRouter);
+app.use("/v1/usuarios", usuariosRouter);
+app.use("/v1/aventura", aventuraRouter);
+app.use("/v1/aventuras", aventurasRouter);
+
+app.listen(port, () => {
+  console.log(`API escuchando en el puerto ${port}`);
+});
